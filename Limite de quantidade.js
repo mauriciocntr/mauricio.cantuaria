74183b3eec3b2c3b3828fc427e67b49adb84de2f
Maurício <99232504+mauriciocntr@users.noreply.github.com>
2022-02-10 01:47:43 -0300

    Delete Limite de quantidade.js

--- Limite de quantidade.js
+++
@@ -1,8 +0,0 @@
-
-let listaDeParticipantes = ["Simone", "Lico", "Antonio",
-"Lisa"];
-let quantidadeDeParticipantes = listaDeParticipantes.length;
-for (let indice = 0; indice < quantidadeDeParticipantes; indice++) {
-	const participanteCorrente = listaDeParticipantes[indice];
-	console.log(participanteCorrente);
-}
