478fa108dfbe21f15f4d925b187c83fa15990f92
Maurício <99232504+mauriciocntr@users.noreply.github.com>
2022-02-28 23:52:59 -0300

    Update Limite de quantidade.js

--- Limite de quantidade.js
+++ Limite de quantidade.js
@@ -1,8 +1,6 @@
-let cadastroDeParticipantes = ["Simone", "Lico", "Antonio", "Lisa"];  
-let quantidadeDeParticipantes = cadastroDeParticipantes.length;  
-if (quantidadeDeParticipantes < 100) {
-    cadastroDeParticipantes.push("Dionisio");  
-console.log(cadastroDeParticipantes);  
-} else {  
-console.log("O cadastro não será permitido por ter excedido o limite");  
+let cadastroDeParticipantes = 99;
+if (cadastroDeParticipantes < 100) {
+    console.log("Permitir cadastro");
+} else {
+    console.log("O cadastro não será permitido por ter excedido o limite");
 }
