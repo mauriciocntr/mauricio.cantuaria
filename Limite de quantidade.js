4b06749137014acd4125ad2ee73b6b69e6501cf7
Maurício <99232504+mauriciocntr@users.noreply.github.com>
2022-04-06 16:05:06 -0300

    Delete Limite de quantidade.js

--- Limite de quantidade.js
+++
@@ -1,6 +0,0 @@
-let cadastroDeParticipantes = 99;
-if (cadastroDeParticipantes < 100) {
-    console.log("Permitir cadastro");
-} else {
-    console.log("O cadastro não será permitido por ter excedido o limite");
-}
