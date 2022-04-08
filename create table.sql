5f907dbb4d9f72868180563766461bb062825599
Maurício <99232504+mauriciocntr@users.noreply.github.com>
2022-04-08 00:30:25 -0300

    Add files via upload

---
+++ create table.sql
@@ -0,0 +1,35 @@
+CREATE TABLE filme (
+	codigo INT NOT NULL AUTO_INCREMENT,
+	titulo VARCHAR(100) NOT NULL,
+	duracao TIME NOT NULL,
+	ano YEAR NOT NULL,
+	capa LONGBLOB,	
+	trailer LONGBLOB,
+    PRIMARY KEY (codigo)
+);
+
+CREATE TABLE genero (
+	codigo INT NOT NULL AUTO_INCREMENT,
+	tipo VARCHAR(100) NOT NULL,
+    PRIMARY KEY (codigo)
+);
+
+CREATE TABLE origem (
+	codigo INT NOT NULL AUTO_INCREMENT,
+	pais VARCHAR(70) NOT NULL,
+	PRIMARY KEY (codigo)
+);
+
+CREATE TABLE elenco (
+	codigo INT NOT NULL AUTO_INCREMENT,
+	nome VARCHAR(70) NOT NULL,
+    idade TINYINT(3) NOT NULL,
+    pais_nascimento VARCHAR(45) NOT NULL,
+	PRIMARY KEY (codigo)
+);
+
+CREATE TABLE diretor (
+	codigo INT NOT NULL AUTO_INCREMENT,
+	nome VARCHAR(70) NOT NULL,
+	PRIMARY KEY (codigo)
+);
